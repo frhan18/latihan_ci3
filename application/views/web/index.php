94fc4f697c53c5cc8b1fc7b61b55f6a1f9458023
Farhan <farhan18apr02@gmail.com>
2022-06-22 06:21:56 +0700

    Menambahkan file web index

---
+++ application/views/web/index.php
@@ -0,0 +1,54 @@
+<!-- jumbotron -->
+<header class="jumbotron">
+
+    <div class="container">
+        <div class="jumbotron-text">
+            <h1>Halo selamat datang di website saya</h1>
+        </div>
+    </div>
+</header>
+
+<!-- end jumbotron -->
+
+
+<main class="content container pt-3">
+
+
+    <section class="article-container">
+
+        <h3 class="mb-0 text-capitalize">
+            Daftar Berita
+        </h3>
+
+        <div class="row align-content-center">
+            <div class="col-lg-10 col-md-10 col-sm-10">
+                <article class="aricle-list">
+                    <div class="article-body">
+                        <div class="article-content__img">
+                            <img src="https://fatkhan.web.id/wp-content/uploads/2016/12/13844228ea43240.png" width="300" class="img-fluid">
+                        </div>
+                        <div class="article-content__bodytext">
+                            <h3>Codeigniter 3</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt omnis tempora error veritatis doloremque nulla provident facilis libero perferendis laboriosam neque officiis, voluptate maiores quis dolore sit quasi placeat laudantium voluptas nisi repellendus qui. Consequatur quia quisquam necessitatibus asperiores animi at ducimus amet sequi esse corrupti. Quos eveniet cumque possimus.</p>
+                        </div>
+                    </div>
+
+                </article>
+            </div>
+            <div class="col-lg-10 col-md-10 col-sm-10">
+                <article class="aricle-list">
+                    <div class="article-body">
+                        <div class="article-content__img">
+                            <img src="https://fatkhan.web.id/wp-content/uploads/2016/12/13844228ea43240.png" width="300" class="img-fluid">
+                        </div>
+                        <div class="article-content__bodytext">
+                            <h3>Codeigniter 3</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt omnis tempora error veritatis doloremque nulla provident facilis libero perferendis laboriosam neque officiis, voluptate maiores quis dolore sit quasi placeat laudantium voluptas nisi repellendus qui. Consequatur quia quisquam necessitatibus asperiores animi at ducimus amet sequi esse corrupti. Quos eveniet cumque possimus.</p>
+                        </div>
+                    </div>
+
+                </article>
+            </div>
+        </div>
+    </section>
+</main>
