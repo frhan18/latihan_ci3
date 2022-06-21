02c9723676af24fd0c26fce87f1967c92eee1439
Farhan <farhan18apr02@gmail.com>
2022-06-22 06:21:01 +0700

    Menambahkan file templating header

---
+++ application/views/templates/header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Membuat web dengan dinamis">
+    <meta name="keywords" content="Menngunakan codeigniter3">
+    <meta name="author" content="Farhan">
+    <meta name="theme-color" content="#ffff">
+    <link rel="stylesheet" href="<?= base_url('assets/') . 'css/bootstrap.css'; ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/') . 'css/style.css'; ?>">
+    <title><?= isset($title) ? $title : 'myweb.com'; ?></title>
+</head>
+
+<body>
+
+
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+        <div class="container">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav  me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+
+                </ul>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Cari Di Web" aria-label="Search">
+                    <button class="btn btn-light" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
