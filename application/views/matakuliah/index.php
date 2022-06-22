c1cdfb4d3ebe9eaf89b9a10404955b826ff07bff
Farhan <farhan18apr02@gmail.com>
2022-06-22 08:01:11 +0700

    Penambahan sweetalert2

--- application/views/matakuliah/index.php
+++ application/views/matakuliah/index.php
@@ -158,3 +158,25 @@
         </div>
     </div>
 <?php endforeach; ?>
+
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<?php if ($this->session->flashdata('message')) : ?>
+    <script>
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.addEventListener('mouseenter', Swal.stopTimer)
+                toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+        })
+
+        Toast.fire({
+            icon: 'success',
+            title: '<?= $this->session->flashdata('message') ?>'
+        })
+    </script>
+<?php endif ?>
